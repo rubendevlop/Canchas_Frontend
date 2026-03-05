a768149df7bdbb17936c020da16bcb4890d99989
Facundo Vera <facundovera.dev@gmail.com>
2026-03-05 16:05:45 -0300

    Agrego animacion de zum a la imagen del Hero

--- src/components/HeroField.jsx
+++ src/components/HeroField.jsx
@@ -3,11 +3,13 @@ import "../css/herofield.css";
 
 const HeroField = () => {
   return (
-    <section
-      className="hero-field  d-flex justify-content-center text-white position-relative"
-      style={{ backgroundImage: `url(${cespedcancha})`, 
-    zIndex:`-10`}}
-    >
+    <section className="hero-field d-flex justify-content-center text-white position-relative">
+
+      <div
+        className="hero-bg"
+        style={{ backgroundImage: `url(${cespedcancha})` }}
+      ></div>
+
       <h1 className="hero-title text-center mt-3">Canchas Futbol - 5</h1>
 
       <div className="position-absolute bottom-0 start-0 px-3 py-5 d-flex flex-column gap-2">
@@ -19,6 +21,7 @@ const HeroField = () => {
           Estacionamiento - Parrilla - Vestuarios
         </span>
       </div>
+
     </section>
   );
 };
