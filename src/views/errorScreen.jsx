b8e204706825a1e95c9823ab7e4b270dbbe33962
Michael Medina <michaelronymedinavelasco@gmail.com>
2026-03-09 17:31:28 -0300

    Página de error 404 básica, más función que ante error 404 del backend redirige a la página de error 404

--- src/views/errorScreen.jsx
+++ src/views/errorScreen.jsx
@@ -1,11 +1,17 @@
 import "../css/error.css"
 
-const errorScreen = () => {
-  return (
-    <>
-      
-    </>
-  )
+const ErrorScreen = () => {
+    return (
+        <main className="main">
+            <div className="err-card">
+                <h1>Error 404</h1>
+                <p>Página no encontrada 😭</p>
+                <button className="back-btn">
+                    Volver al inicio
+                </button>
+            </div>
+        </main>
+    )
 }
 
-export default errorScreen
+export default ErrorScreen
