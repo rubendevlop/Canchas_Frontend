a234438bc6642dc7b48d97cc5e38eda22af1ca34
Cristian Bustos <bustoscristianmiguel@gmail.com>
2026-03-11 16:53:53 -0300

    hice los arreglos del merge de la rama login

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { AdminDashboard } from '../views/AdminDashboard'; 
+import LoginScreen from '../views/auth/LoginScreen';
+import HomeScreen from '../views/HomeScreen';
 
 export const AppRouter = () => {
   return (
     <Routes>
-     
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
       <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   );
